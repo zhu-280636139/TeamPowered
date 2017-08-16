2450e2e007ac8334056cdeff07a7747b49858cfe
jun zhu <junyuzhu000@outlook.com>
2017-08-16 11:15:19 -0700

    Split Name into first and last name.

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -8,7 +8,8 @@ namespace TeamPowered
 {
     class Person
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
     }
 }
